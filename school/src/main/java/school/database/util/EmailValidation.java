b398524443adc14b4b63a055d673f653d263195a
ckarlson001 <89924386+ckarlson001@users.noreply.github.com>
2022-11-01 09:25:28 -0400

    homework 10/31

--- school/src/main/java/school/database/util/EmailValidation.java
+++ school/src/main/java/school/database/util/EmailValidation.java
@@ -4,15 +4,37 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailValidation {
-	public static boolean isValidEmail(String email) {
+	public boolean isValidEmail(String email) {
 		Pattern pattern = Pattern.compile("(([a-z]|[A-Z]|[0-9])+)(@)(.+)(\\.)(([a-z]|[A-Z]){3})");
 		Matcher matcher = pattern.matcher(email);
 		boolean matchFound = matcher.matches();
 		return matchFound;
 	}
-	
-	public static void main(String args[]) {
-		String email = "asdg630jdf@xcbk.ifm";
-		System.out.println(isValidEmail(email));
+
+	public boolean isValidPhoneNumber(String number) {
+		if (number == null || number.equals("")) {
+			return false;
+		}
+
+		int numDigits = 0;
+		for (int i = 0; i < number.length(); i++) {
+			if (Character.isDigit(number.charAt(i))) {
+				numDigits++;
+			}
+		}
+		if (numDigits != 9)
+			return false;
+
+		for (int i = 0; i < number.length(); i++) {
+			if (!Character.isDigit(number.charAt(i)) && number.charAt(i) != '-' && number.charAt(i) != '.'
+					&& number.charAt(i) != ' ')
+				return false;
+		}
+
+		if (number.substring(0, 3).equals("555"))
+			return false;
+
+		return true;
 	}
+
 }
