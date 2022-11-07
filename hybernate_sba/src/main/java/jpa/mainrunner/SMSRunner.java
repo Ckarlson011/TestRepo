f946c652bf8b9769da50e064f841512788bc697c
ckarlson001 <89924386+ckarlson001@users.noreply.github.com>
2022-11-07 10:21:29 -0500

    wip

--- hybernate_sba/src/main/java/jpa/mainrunner/SMSRunner.java
+++ hybernate_sba/src/main/java/jpa/mainrunner/SMSRunner.java
@@ -14,7 +14,7 @@ public class SMSRunner {
 		StudentDAO studentDAO = new StudentDAO();
 		CourseDAO courseDAO = new CourseDAO();
 		Scanner scnr = new Scanner(System.in);
-		while(true) {
+		while (true) {
 			System.out.println("Are you a(n)");
 			System.out.println();
 			System.out.println("  1. Student");
@@ -22,54 +22,89 @@ public class SMSRunner {
 			System.out.println();
 			System.out.println("Please, enter 1 or 2.");
 			int selection;
-			if (scnr.hasNextInt()) selection = scnr.nextInt();
-			else selection = 3;
+			if (scnr.hasNextInt())
+				selection = scnr.nextInt();
+			else
+				selection = 3;
 			scnr.nextLine();
 			if (selection == 1) {
 				System.out.println("Enter Your Email:");
 				String email = scnr.nextLine();
 				System.out.println("Enter Your Password");
 				String pword = scnr.nextLine();
-				if(studentDAO.validateStudent(email, pword)) {
+				if (studentDAO.validateStudent(email, pword)) {
 					System.out.println("MY COURSES:");
 					List<Course> myCourses = studentDAO.getStudentCourses(email);
 					for (Course thisCourse : myCourses) {
-						System.out.println(thisCourse.getCId() + " | " + thisCourse.getCInstructorName() + " | " + thisCourse.getCName());
+						System.out.println(thisCourse.getCId() + " | " + thisCourse.getCInstructorName() + " | "
+								+ thisCourse.getCName());
 					}
 					System.out.println();
 					System.out.println("Would you like to:");
 					System.out.println("  1. Register to Class");
 					System.out.println("  2. Logout");
-					while(true) {
-						if (scnr.hasNextInt()) selection = scnr.nextInt();
-						else selection = 3;
+					while (true) {
+						if (scnr.hasNextInt())
+							selection = scnr.nextInt();
+						else
+							selection = 3;
 						scnr.nextLine();
-						if (selection == 1 || selection == 2) break;
-						else System.out.println("Try Again");
+						if (selection == 1 || selection == 2)
+							break;
+						else
+							System.out.println("Try Again");
 					}
 					if (selection == 1) {
-						System.out.println("ALL COURSES:");
+						boolean keepRunning = true;
+						while (keepRunning) {
+							System.out.println("ALL COURSES:");
+							List<Course> allCourses = courseDAO.getAllCourses();
+							for (Course thisCourse : allCourses) {
+								System.out.println(thisCourse.getCId() + " | " + thisCourse.getCInstructorName() + " | "
+										+ thisCourse.getCName());
+							}
+							System.out.println("\nWhich course would you like to register for");
+							if (scnr.hasNextInt()) {
+								selection = scnr.nextInt();
+							} else {
+								System.out.println("bad selection");
+								scnr.nextLine();
+								continue;
+							}
+							scnr.nextLine();
+							boolean badSelection = false;
+							for (Course aCourse : myCourses) {
+								if (aCourse.getCId() == (Integer)selection) {
+									System.out.println("You are already registered in that course!");
+									badSelection = true;
+								}
+							}
+							if (badSelection = false) {
+								StudentDAO studentDAO2 = new StudentDAO();
+								studentDAO2.registerStudentToCourse(email, selection);
+								keepRunning = false;
+							} else {
+								System.out.println("Please try again");
+							}
+						}
 						List<Course> allCourses = courseDAO.getAllCourses();
 						for (Course thisCourse : allCourses) {
-							System.out.println(thisCourse.getCId() + " | " + thisCourse.getCInstructorName() + " | " + thisCourse.getCName());
+							System.out.println(thisCourse.getCId() + " | " + thisCourse.getCInstructorName() + " | "
+									+ thisCourse.getCName());
 						}
-						System.out.println("\nWhich course would you like to register for")//TODO
-					}
-					else {
+						System.out.println("goodbye");
+					} else {
 						System.out.println("Goodbye!");
 						break;
 					}
-				}
-				else {
+				} else {
 					System.out.println("Invalid Entries\nGoodbye!");
 					break;
 				}
-			}
-			else if (selection == 2) {
+			} else if (selection == 2) {
 				System.out.println("Goodbye!");
 				break;
-			}
-			else {
+			} else {
 				System.out.println("Incorrect selection, please try again.\n");
 			}
 		}
